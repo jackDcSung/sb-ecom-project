fa23843118df3f21564e5453ed6a74a2b766b334
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-07 12:26:47 +0800

    Implementing Pagination and Sorting in Our Ecommerce Project

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,61 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long productId;
+
+    private  String productName;
+
+
+    private  String image;
+
+    private  String description;
+
+
+    private  Integer quantity;
+
+    private  double price;//100
+
+    private  double discount;//25
+
+
+    private  double specialPrice;//75
+
+
+
+
+
+
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private  Category category;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
