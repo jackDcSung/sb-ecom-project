4abbce3416b4390f3c3f5df823627b04c8880622
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-09 05:37:30 +0800

    Deleting Product from the Cart

--- src/main/java/com/ecommerce/project/service/CartService.java
+++ src/main/java/com/ecommerce/project/service/CartService.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.service;
 
 
 import com.ecommerce.project.payload.CartDTO;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -15,10 +16,14 @@ public interface CartService {
     List<CartDTO> getAllCarts();
 
 
-    CartDTO getCart(String emailId,Long cartId);
+    CartDTO getCart(String emailId, Long cartId);
 
 
     @Transactional
     CartDTO updateProductQuantityInCart(Long productId, int quantity);
+
+
+    @Query("delete  from  CartItem  ci where ci.cart=?1 and ci.product.id=?2")
+    String deleteProductFromCart(Long cartId, Long productId);
 
 }
