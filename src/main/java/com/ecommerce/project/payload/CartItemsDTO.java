466afed9495ebfb933e3cf86ec438dfa2ce96a41
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-09 00:36:14 +0800

    DTOs and Repositories

---
+++ src/main/java/com/ecommerce/project/payload/CartItemsDTO.java
@@ -0,0 +1,40 @@
+package com.ecommerce.project.payload;
+
+public class CartItemsDTO {
+
+
+private  Long cartItemId;
+
+private  CartDTO cart;
+
+private  ProductDTO productDTO;
+
+
+private  Integer quantity;
+
+private  Double discount;
+
+private  Double productPrice;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
