8532ca3410ecaa0c8678d6f136e887f209c465d4
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-09 08:21:31 +0800

    Creating  Getting Addresses API

--- src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -44,6 +45,18 @@ public class AddressServiceImpl implements AddressService {
 
         return modelMapper.map(savedAddress, AddressDTO.class);
 
+
+    }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+
+        List<Address> addresses = addressRepository.findAll();
+
+
+        return addresses.stream()
+                  .map(myadddress -> modelMapper.map(myadddress, AddressDTO.class))
+                  .toList();
 
     }
 }
